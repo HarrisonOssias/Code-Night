cf4aeeb6b4ac9b38ff27114f814d504a96b5b7a1
Harrison Ossias <ossiash@mcmaster.ca>
2020-11-01 15:48:12 -0500

    Added custom and creat react-app (yarn) packages

---
+++ Calendar/webpack.config.js
@@ -0,0 +1,36 @@
+// Load the plugin in your webpack.config.js file.
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      // Add HTML loader to serve HTML files.
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  },
+  // Call the Plugin here.
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
--- Challenge_1/src/app/index.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import moment from "moment";
-import Calendar from "./calendar";
-import "./styles.css";
-
-export default function () {
-  const [selectedDate, setSelectedDate] = useState(moment());
-  return <Calendar value={selectedDate} onChange={setSelectedDate} />;
-}
